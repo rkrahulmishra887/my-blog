17c897e4f67b9d037a922262daa70fd936254f63
rkrahulmishra887 <rkrahulmishra88714@gmail.com>
2020-07-16 23:02:53 +0530

    first commit

---
+++ src/build/precache-manifest.e9b15418ba3e9b6105b32a84d27d32d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49989d79b1b624df54fdfe2e3dd9b2b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "591ff552c11d85d8945a",
+    "url": "/static/css/main.97a72765.chunk.css"
+  },
+  {
+    "revision": "3de77b5fe91ec6868a19",
+    "url": "/static/js/2.3bd67d22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3bd67d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591ff552c11d85d8945a",
+    "url": "/static/js/main.925b7cde.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
